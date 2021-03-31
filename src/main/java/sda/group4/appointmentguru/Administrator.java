03bec4b87f9d6d67b934dfcf1231f33a9e24b863
violeta0408 <violeta0408@inbox.lv>
2021-03-31 22:27:22 +0300

    Create class insertDoctorDetails; updateDoctorDetail

--- src/main/java/sda/group4/appointmentguru/Administrator.java
+++ src/main/java/sda/group4/appointmentguru/Administrator.java
@@ -2,7 +2,6 @@ package sda.group4.appointmentguru;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.sql.Time;
 import java.util.Scanner;
 
 public class Administrator {
@@ -31,32 +30,8 @@ public class Administrator {
                     System.out.println("All records in the database");
                     Doctors.printAllRecordDoctor(connection);
 
-                    //to get information from console about doctor
-                    Scanner scanner1 = new Scanner(System.in);
-                    System.out.println("Enter doctor's medical speciality:");
-                    String doctor_medical_speciality = scanner1.nextLine();
-                    System.out.println("Enter doctor's name:");
-                    String doctor_name = scanner1.nextLine();
-                    System.out.println("Enter doctor's surname:");
-                    String doctor_surname = scanner1.nextLine();
-                    System.out.println("Enter doctor's room number:");
-                    Integer doctor_room_number = scanner1.nextInt();
-                    System.out.println("Enter the start time of the doctor's working hours (hh:mm:ss): ");
-                    Time doctor_work_start_time = Time.valueOf(scanner1.next());
-                    System.out.println("Enter the end time of the doctor's working hours (hh:mm:ss): ");
-                    Time doctor_work_end_time = Time.valueOf(scanner1.next());
-                    System.out.println("Enter the price of appointment:");
-                    Double doctor_visit_price = scanner1.nextDouble();
-
-                    //to load information about a doctor to Doctor table
-                    System.out.println("Information about doctor is loading in database");
-                    Doctors.insertIntoDoctorTable(connection,
-                            doctor_medical_speciality,
-                            doctor_name, doctor_surname,
-                            doctor_room_number,
-                            doctor_work_start_time,
-                            doctor_work_end_time,
-                            doctor_visit_price); //insert information in table
+                    //to insert information about Doctor
+                    Doctors.insertDoctorDetails(connection);
 
                     break;
                 case 2:
@@ -66,42 +41,8 @@ public class Administrator {
                     System.out.println("All records in the database");
                     Doctors.printAllRecordDoctor(connection);
 
-                    //to update information in Doctor table, if it was some mistake before
-                    System.out.println("Please enter doctor id code, to update records about doctor: ");
-                    int doctorIdToUpdate = scanner.nextInt();
-
-                    //parbaudit vai pieejams ar tadu id, vai ari prasit vadit ne id, bet ko citu - ko?, id, laikam ok
-
-                    //te var būt izvēle par to, kuru informāciju vadisim pa jaunu
-                    //to get new information from console about doctor
-                    Scanner scanner2 = new Scanner(System.in);
-                    System.out.println("Enter doctor's medical speciality:");
-                    String doctor_medical_speciality_Update = scanner2.nextLine();
-                    System.out.println("Enter doctor's name:");
-                    String doctor_name_Update = scanner2.nextLine();
-                    System.out.println("Enter doctor's surname:");
-                    String doctor_surname_Update = scanner2.nextLine();
-                    System.out.println("Enter doctor's room number:");
-                    Integer doctor_room_number_Update = scanner2.nextInt();
-                    System.out.println("Enter the start time of the doctor's working hours (hh:mm:ss): ");
-                    Time doctor_work_start_time_Update = Time.valueOf(scanner2.next());
-                    System.out.println("Enter the end time of the doctor's working hours (hh:mm:ss): ");
-                    Time doctor_work_end_time_Update = Time.valueOf(scanner2.next());
-                    System.out.println("Enter the price of appointment:");
-                    Double doctor_visit_price_Update = scanner2.nextDouble();
-
-                    // to update information in Doctor table, if it was some mistake before
-                    Doctors.updateRecordDoctor(connection,
-                            doctor_medical_speciality_Update,
-                            doctor_name_Update,
-                            doctor_surname_Update,
-                            doctor_room_number_Update,
-                            doctor_work_start_time_Update,
-                            doctor_work_end_time_Update,
-                            doctor_visit_price_Update,
-                            doctorIdToUpdate);
-
-                    //pec tam, kad ieraksts par arstu atjaunots, to jaatjauno ari lielas tabula, kur glabajas viss ar pierakstiem
+                    //to update information about Doctor
+                    Doctors.updateDoctorDetails(connection);
 
                     break;
                 case 3:
@@ -121,6 +62,7 @@ public class Administrator {
                     System.out.println("Please enter doctor id code, to delete records about doctor: ");
                     int idDoctorCodeToDelete = scanner.nextInt();
                     Doctors.deleteRecordDoctor(connection, idDoctorCodeToDelete);
+
                     /*
                     OperationFile.deleteRecordAppointment(connection, idDoctorCodeToDelete);
                      */
--- src/main/java/sda/group4/appointmentguru/Doctors.java
+++ src/main/java/sda/group4/appointmentguru/Doctors.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 
 public class Doctors {
 
-
     // to create table with Doctors, who work in our hospital
     public static void createTableDoctor(Connection connection) throws SQLException {
         String sql = "CREATE TABLE IF NOT EXISTS doctor(" +
@@ -21,28 +20,6 @@ public class Doctors {
         //Try with resources
         try (Statement statement = connection.createStatement()) {
             statement.execute(sql);
-        }
-    }
-
-    // to get all records about Doctors, who work in our hospital
-    public static void printAllRecordDoctor(Connection connection) throws SQLException {
-        String sql = "SELECT * from doctor";
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                Integer id_doctor_code = resultSet.getInt("id_doctor_code");
-                String doctor_medical_speciality = resultSet.getString("doctor_medical_speciality");
-                String doctor_name = resultSet.getString("doctor_name");
-                String doctor_surname = resultSet.getString("doctor_surname");
-                Integer doctor_room_number = resultSet.getInt("doctor_room_number");
-                Time doctor_work_start_time = resultSet.getTime("doctor_work_start_time");
-                Time doctor_work_end_time = resultSet.getTime("doctor_work_end_time");
-                Double doctor_visit_price = resultSet.getDouble("doctor_visit_price");
-
-                System.out.println(id_doctor_code + " | " + doctor_medical_speciality + " | "
-                        + doctor_name + " | " + doctor_surname + " | " + doctor_room_number + " | "
-                        + doctor_work_start_time + " | " + doctor_work_end_time + " | " + doctor_visit_price);
-            }
         }
     }
 
@@ -72,48 +49,6 @@ public class Doctors {
             }
         }
     }
-
-    // to delete information in Doctor table, if Doctor doesn't work more
-    public static void deleteRecordDoctor(Connection connection, int id_doctor_code) throws SQLException {
-        String sql = "DELETE from doctor where id_doctor_code=?";
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setInt(1, id_doctor_code);
-            int affectedRows = statement.executeUpdate();
-            if (affectedRows > 0) {
-                System.out.printf("Record about Doctor with id %s was successfully deleted \n", id_doctor_code);
-            }
-        }
-    }
-
-    public static void insertDoctorDetails (Connection connection) throws SQLException {
-
-        //to get information from console about doctor
-        Scanner scanner1 = new Scanner(System.in);
-        System.out.println("Enter doctor's medical speciality:");
-        String doctor_medical_speciality = scanner1.nextLine();
-        System.out.println("Enter doctor's name:");
-        String doctor_name = scanner1.nextLine();
-        System.out.println("Enter doctor's surname:");
-        String doctor_surname = scanner1.nextLine();
-        System.out.println("Enter doctor's room number:");
-        Integer doctor_room_number = scanner1.nextInt();
-        System.out.println("Enter the start time of the doctor's working hours (hh:mm:ss): ");
-        Time doctor_work_start_time = Time.valueOf(scanner1.next());
-        System.out.println("Enter the end time of the doctor's working hours (hh:mm:ss): ");
-        Time doctor_work_end_time = Time.valueOf(scanner1.next());
-        System.out.println("Enter the price of appointment:");
-        Double doctor_visit_price = scanner1.nextDouble();
-
-        //to load information about a doctor to Doctor table
-        System.out.println("Information about doctor is loading in database");
-        Doctors.insertIntoDoctorTable(connection,
-                doctor_medical_speciality,
-                doctor_name, doctor_surname,
-                doctor_room_number,
-                doctor_work_start_time,
-                doctor_work_end_time,
-                doctor_visit_price); //insert information in table
-        }
 
     // to update information in Doctor table, if it was some mistake before
     public static void updateRecordDoctor(Connection connection,
@@ -145,4 +80,108 @@ public class Doctors {
             }
         }
     }
+
+    // to delete information in Doctor table, if Doctor doesn't work more
+    public static void deleteRecordDoctor(Connection connection, int id_doctor_code) throws SQLException {
+        String sql = "DELETE from doctor where id_doctor_code=?";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, id_doctor_code);
+            int affectedRows = statement.executeUpdate();
+            if (affectedRows > 0) {
+                System.out.printf("Record about Doctor with id %s was successfully deleted \n", id_doctor_code);
+            }
+        }
+    }
+
+    // to get all records about Doctors, who work in our hospital
+    public static void printAllRecordDoctor(Connection connection) throws SQLException {
+        String sql = "SELECT * from doctor";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                Integer id_doctor_code = resultSet.getInt("id_doctor_code");
+                String doctor_medical_speciality = resultSet.getString("doctor_medical_speciality");
+                String doctor_name = resultSet.getString("doctor_name");
+                String doctor_surname = resultSet.getString("doctor_surname");
+                Integer doctor_room_number = resultSet.getInt("doctor_room_number");
+                Time doctor_work_start_time = resultSet.getTime("doctor_work_start_time");
+                Time doctor_work_end_time = resultSet.getTime("doctor_work_end_time");
+                Double doctor_visit_price = resultSet.getDouble("doctor_visit_price");
+
+                System.out.println(id_doctor_code + " | " + doctor_medical_speciality + " | "
+                        + doctor_name + " | " + doctor_surname + " | " + doctor_room_number + " | "
+                        + doctor_work_start_time + " | " + doctor_work_end_time + " | " + doctor_visit_price);
+            }
+        }
+    }
+
+
+    public static void insertDoctorDetails(Connection connection) throws SQLException {
+        //to get information from console about doctor
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Enter doctor's medical speciality:");
+        String doctor_medical_speciality = scanner1.nextLine();
+        System.out.println("Enter doctor's name:");
+        String doctor_name = scanner1.nextLine();
+        System.out.println("Enter doctor's surname:");
+        String doctor_surname = scanner1.nextLine();
+        System.out.println("Enter doctor's room number:");
+        Integer doctor_room_number = scanner1.nextInt();
+        System.out.println("Enter the start time of the doctor's working hours (hh:mm:ss): ");
+        Time doctor_work_start_time = Time.valueOf(scanner1.next());
+        System.out.println("Enter the end time of the doctor's working hours (hh:mm:ss): ");
+        Time doctor_work_end_time = Time.valueOf(scanner1.next());
+        System.out.println("Enter the price of appointment:");
+        Double doctor_visit_price = scanner1.nextDouble();
+
+        //to load information about a doctor to Doctor table
+        System.out.println("Information about doctor is loading in database");
+        Doctors.insertIntoDoctorTable(connection,
+                doctor_medical_speciality,
+                doctor_name, doctor_surname,
+                doctor_room_number,
+                doctor_work_start_time,
+                doctor_work_end_time,
+                doctor_visit_price); //insert information in table
+
+    }
+
+    public static void updateDoctorDetails(Connection connection) throws SQLException {                    //te var būt izvēle par to, kuru informāciju vadisim pa jaunu
+        //to update information in Doctor table, if it was some mistake before
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter doctor id code, to update records about doctor: ");
+        int doctorIdToUpdate = scanner.nextInt();
+
+        //parbaudit vai pieejams ar tadu id, vai ari prasit vadit ne id, bet ko citu - ko?, id, laikam ok
+
+        //to get new information from console about doctor
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Enter doctor's medical speciality:");
+        String doctor_medical_speciality_Update = scanner2.nextLine();
+        System.out.println("Enter doctor's name:");
+        String doctor_name_Update = scanner2.nextLine();
+        System.out.println("Enter doctor's surname:");
+        String doctor_surname_Update = scanner2.nextLine();
+        System.out.println("Enter doctor's room number:");
+        Integer doctor_room_number_Update = scanner2.nextInt();
+        System.out.println("Enter the start time of the doctor's working hours (hh:mm:ss): ");
+        Time doctor_work_start_time_Update = Time.valueOf(scanner2.next());
+        System.out.println("Enter the end time of the doctor's working hours (hh:mm:ss): ");
+        Time doctor_work_end_time_Update = Time.valueOf(scanner2.next());
+        System.out.println("Enter the price of appointment:");
+        Double doctor_visit_price_Update = scanner2.nextDouble();
+
+        // to update information in Doctor table, if it was some mistake before
+        Doctors.updateRecordDoctor(connection,
+                doctor_medical_speciality_Update,
+                doctor_name_Update,
+                doctor_surname_Update,
+                doctor_room_number_Update,
+                doctor_work_start_time_Update,
+                doctor_work_end_time_Update,
+                doctor_visit_price_Update,
+                doctorIdToUpdate);
+    }
+
+
 }

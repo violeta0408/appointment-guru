aa4b8660a82a589e0a7ce6e08be3f2927a6ea649
violeta0408 <violeta0408@inbox.lv>
2021-04-08 09:36:47 +0300

    create method isDoctorIDInDB and changes with this moment

--- src/main/java/sda/group4/appointmentguru/Doctors.java
+++ src/main/java/sda/group4/appointmentguru/Doctors.java
@@ -158,91 +158,116 @@ public class Doctors {
         Scanner scanner = new Scanner(System.in);
         System.out.println("To update records about a doctor, please enter doctor's ID code: ");
         int doctorIdToUpdate = scanner.nextInt();
+        //to check information about Doctor: is Doctor in DB (1) or not (0)
+        if (Doctors.isDoctorIDInDB(connection,doctorIdToUpdate)==1) {
+            //to get new information from console about doctor
+            Scanner scanner2 = new Scanner(System.in);
+            System.out.println("Enter doctor's medical speciality:");
+            String doctor_medical_speciality_Update = scanner2.nextLine();
+            System.out.println("Enter doctor's name:");
+            String doctor_name_Update = scanner2.nextLine();
+            System.out.println("Enter doctor's surname:");
+            String doctor_surname_Update = scanner2.nextLine();
+            System.out.println("Enter doctor's room number:");
+            Integer doctor_room_number_Update = scanner2.nextInt();
+            System.out.println("Enter the start time of the doctor's working hours (hh:mm:ss): ");
+            Time doctor_work_start_time_Update = Time.valueOf(scanner2.next());
+            System.out.println("Enter the end time of the doctor's working hours (hh:mm:ss): ");
+            Time doctor_work_end_time_Update = Time.valueOf(scanner2.next());
+            System.out.println("Enter the price of appointment:");
+            Double doctor_visit_price_Update = scanner2.nextDouble();
 
-        //parbaudit vai pieejams ar tadu id, vai ari prasit vadit ne id, bet ko citu - ko?, id, laikam ok
+            // to update information in Doctor table, if it was some mistake before
+            Doctors.updateRecordDoctor(connection,
+                    doctor_medical_speciality_Update,
+                    doctor_name_Update,
+                    doctor_surname_Update,
+                    doctor_room_number_Update,
+                    doctor_work_start_time_Update,
+                    doctor_work_end_time_Update,
+                    doctor_visit_price_Update,
+                    doctorIdToUpdate);
+        }else{
+            System.out.println("Doctor with this ID is not in our DB! It is not possible to make information updating.");
+        }
+    }
 
-        //to get new information from console about doctor
-        Scanner scanner2 = new Scanner(System.in);
-        System.out.println("Enter doctor's medical speciality:");
-        String doctor_medical_speciality_Update = scanner2.nextLine();
-        System.out.println("Enter doctor's name:");
-        String doctor_name_Update = scanner2.nextLine();
-        System.out.println("Enter doctor's surname:");
-        String doctor_surname_Update = scanner2.nextLine();
-        System.out.println("Enter doctor's room number:");
-        Integer doctor_room_number_Update = scanner2.nextInt();
-        System.out.println("Enter the start time of the doctor's working hours (hh:mm:ss): ");
-        Time doctor_work_start_time_Update = Time.valueOf(scanner2.next());
-        System.out.println("Enter the end time of the doctor's working hours (hh:mm:ss): ");
-        Time doctor_work_end_time_Update = Time.valueOf(scanner2.next());
-        System.out.println("Enter the price of appointment:");
-        Double doctor_visit_price_Update = scanner2.nextDouble();
-
-        // to update information in Doctor table, if it was some mistake before
-        Doctors.updateRecordDoctor(connection,
-                doctor_medical_speciality_Update,
-                doctor_name_Update,
-                doctor_surname_Update,
-                doctor_room_number_Update,
-                doctor_work_start_time_Update,
-                doctor_work_end_time_Update,
-                doctor_visit_price_Update,
-                doctorIdToUpdate);
+    //to check information about Doctor: is Doctor in DB (1) or not (0)
+    public static int isDoctorIDInDB(Connection connection, int id_doctor_code_selected) throws SQLException {
+        String sql = "SELECT id_doctor_code from doctor where id_doctor_code=?";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, id_doctor_code_selected);
+            ResultSet resultSet = statement.executeQuery();
+            int id_doctor_code_result = 0;
+            while (resultSet.next()) {
+                Integer id_doctor_code = resultSet.getInt("id_doctor_code");
+                if (id_doctor_code.equals(id_doctor_code_selected)) {
+                    id_doctor_code_result = 1;
+                } else {
+                    id_doctor_code_result = id_doctor_code_result;
+                }
+            }
+            return id_doctor_code_result;
+        }
     }
 
 
     //
     public static void doctorRequest(Connection connection) throws SQLException {
-        //to identify doctor
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter your ID code");
-        int selected_id_doctor_code = scanner.nextInt();
-        //te jāliek pārbaude vai tāds arsts ir- bet izstikt bez pārbaudes, tad saraksts tukss
-
         //start with while - to can run application many times
         int runApplication = 1;
         while (runApplication == 1) {
-            //information about what is possible to do in our application
-            System.out.println("What would you like to do? Please enter 1, 2, 3 or 4: ");
-            System.out.println("  1 - to see all your appointments");
-            System.out.println("  2 - to see all your appointments for today");
-            System.out.println("  3 - to see all your appointments for the selected day");
-            System.out.println("  4 - to see next appointment details");
+            //to identify doctor
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please enter your ID code");
+            int selected_id_doctor_code = scanner.nextInt();
 
-            //enter choose what you will to do in our application
-            System.out.println("Enter your choice: ");
-            int selectedChoose = scanner.nextInt();
+            //to check information about Doctor: is Doctor in DB (1) or not (0)
+            if (Doctors.isDoctorIDInDB(connection, selected_id_doctor_code) == 1) {
+                //information about what is possible to do in our application
+                System.out.println("What would you like to do? Please enter 1, 2, 3 or 4: ");
+                System.out.println("  1 - to see all your appointments");
+                System.out.println("  2 - to see all your appointments for today");
+                System.out.println("  3 - to see all your appointments for the selected day");
+                System.out.println("  4 - to see next appointment details");
 
-            //operation after selectedChoose depend of choose - start process
-            switch (selectedChoose) {
-                case 1: //1 - to see all appointment as a doctor
-                    System.out.println("All your appointments:");
-                    Appointment.viewMyAppointmentDoctor(connection, selected_id_doctor_code);
-                    break;
+                //enter choose what you will to do in our application
+                System.out.println("Enter your choice: ");
+                int selectedChoose = scanner.nextInt();
 
-                case 2: //2 - to see all appointments for today as a doctor
-                    Date dateToday = Date.valueOf(LocalDate.now());
-                    System.out.printf("All your appointments for today (%s): \n", dateToday);
-                    Appointment.viewAppointmentForOneDayDoctor(connection, selected_id_doctor_code, dateToday);
-                    break;
+                //operation after selectedChoose depend of choose - start process
+                switch (selectedChoose) {
+                    case 1: //1 - to see all appointment as a doctor
+                        System.out.println("All your appointments:");
+                        Appointment.viewMyAppointmentDoctor(connection, selected_id_doctor_code);
+                        break;
 
-                case 3: //3 - to see all appointments for the selected day as a doctor
-                    System.out.println("Please enter the date for which you would like to see your appointments. Enter the date using format YYYY-MM-DD");
-                    Date dateSelected = Date.valueOf(scanner.next());
-                    //?parbaude uz ievaditu datumu
-                    System.out.println("All your appointments for the selected day: ");
-                    Appointment.viewAppointmentForOneDayDoctor(connection, selected_id_doctor_code, dateSelected);
-                    break;
+                    case 2: //2 - to see all appointments for today as a doctor
+                        Date dateToday = Date.valueOf(LocalDate.now());
+                        System.out.printf("All your appointments for today (%s): \n", dateToday);
+                        Appointment.viewAppointmentForOneDayDoctor(connection, selected_id_doctor_code, dateToday);
+                        break;
 
-                case 4: //4 - to see next appointment details as a doctor
-                    //!!!so vel jataisa
-                    System.out.println("Your next appointment is: ");
-                    //te, jāsataisa, lai būtu next appointment
-                    //Appointment.viewMyNextAppointmentDoctor(connection, selected_id_doctor_code));
-                    break;
+                    case 3: //3 - to see all appointments for the selected day as a doctor
+                        System.out.println("Please enter the date for which you would like to see your appointments. Enter the date using format YYYY-MM-DD");
+                        Date dateSelected = Date.valueOf(scanner.next());
+                        //?parbaude uz ievaditu datumu
+                        System.out.println("All your appointments for the selected day: ");
+                        Appointment.viewAppointmentForOneDayDoctor(connection, selected_id_doctor_code, dateSelected);
+                        break;
 
-                default: // all other choose: perform if and only if none of the above conditions are met
-                    System.out.println("Please enter a valid number depending on what you would like to do - 1, 2, 3 or 4.");
+                    case 4: //4 - to see next appointment details as a doctor
+                        //!!!so vel jataisa
+                        System.out.println("Your next appointment is: ");
+                        //te, jāsataisa, lai būtu next appointment
+                        //Appointment.viewMyNextAppointmentDoctor(connection, selected_id_doctor_code));
+                        break;
+
+                    default: // all other choose: perform if and only if none of the above conditions are met
+                        System.out.println("Please enter a valid number depending on what you would like to do - 1, 2, 3 or 4.");
+                }
+            } else {
+                System.out.println("Doctor with this ID is not in our DB!");
             }
             System.out.println("If you would like to do more operations - press 1");
             runApplication = scanner.nextInt();
